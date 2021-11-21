3108383851ce713e9346de1e4c7ed03c2f62fe2d
Oli G <oli@Macintosh.fritz.box>
2021-11-21 17:59:25 +0100

    bird template added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/bird/<name>")
+def bird(name):
+    return render_template('user.html', name=name)
+
+@app.route("/wiki")
+def wiki():
+    return redirect('http://wikipedia.org')
 
 if __name__ == "__main__":
     app.run()
