e2e39b5f072dbc9eccf8fe060f5c013e54fe36c3
Oli G <oli@Macintosh.fritz.box>
2021-11-29 21:56:29 +0100

    Feature: favicon + flask-moment + locale

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,7 +17,7 @@ def internal_server_error(e):
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow(), locale='es')
 
 @app.route("/bird/<name>")
 def bird(name):
